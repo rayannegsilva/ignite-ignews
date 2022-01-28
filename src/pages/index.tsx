4164486d02cbb9696d09cb1376fb566c0af57b3f
Rayanne Giló da Silva <rgilodasilva@gmail.com>
2022-01-28 13:59:25 -0300

    Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({ product }: HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world</h1>
+          <h1> about the <span>React</span> world</h1>
           <p>Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
