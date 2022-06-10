027bb471a02e9a1ba0f3f2df0a1439401469cb6a
Rayanne Giló da Silva <rgilodasilva@gmail.com>
2022-06-09 21:00:46 -0300

    imagem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img className={styles.logo} src='/images/logo.svg' alt='ig.news'></img>
+        <Image className={styles.logo} src='/images/logo.svg' alt='ig.news'></Image>
         <nav>
           <ActiveLink activeClassName={styles.active} href='/' >
             <a>Home</a>
